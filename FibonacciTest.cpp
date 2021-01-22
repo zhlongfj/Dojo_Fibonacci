bf544cee9c462de2472631cb3b2ed427ea8a6839
zhlongfj <zhlongfj@sina.com>
2021-01-22 19:12:24 +0800

    refactor the test

--- FibonacciTest.cpp
+++ FibonacciTest.cpp
@@ -10,9 +10,7 @@ SCENARIO("Fibonacci", "Fibonacci") {
     GIVEN("calculate Fibonacci") {
         WHEN("input 0") {
             THEN("return 0") {
-                Fibonacci fibonacci;
-                int64_t ret = fibonacci.calculate(0);
-                CHECK(ret == 0);
+                CHECK(Fibonacci().calculate(0) == 0);
             }
         }
     }
